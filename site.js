2a2ae652cc0215553b7bc9af5e0151e91b375063
Felipe Lobo <felipeblobo@gmail.com>
2020-12-15 22:50:01 -0300

    Finalizando o site

---
+++ site.js
@@ -0,0 +1,6 @@
+var carouselCities = document.querySelector('#carouselCities')
+var carouselCities = new bootstrap.Carousel(carouselCities, {
+  interval: 2000,
+  wrap: false,
+  keyboard: true
+})
